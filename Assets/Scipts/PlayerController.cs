a96f29f454e09837ec96b99bb903006e17544363
Hiikun <dark_makai@hotmail.com>
2018-10-23 20:12:52 -0300

    first commit

---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float jump;
+	float moveVelocity;
+
+	Rigidbody2D playerBody;
+
+	bool isGrounded = true;
+
+	void Start()
+	{
+		playerBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			if (this.isGrounded)
+				playerBody.velocity = new Vector2(playerBody.velocity.x, jump);
+		}
+
+		moveVelocity = 0;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			moveVelocity = -speed;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			moveVelocity = speed;
+		}
+
+		playerBody.velocity = new Vector2(moveVelocity, playerBody.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		isGrounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		isGrounded = false;
+	}
+}
